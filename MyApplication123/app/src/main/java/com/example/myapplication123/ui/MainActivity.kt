6d76d95f0c6ee6b74279d68943736759ddafbdf1
김현재 <41482946+galug@users.noreply.github.com>
2022-07-06 12:11:16 +0900

    Update MainActivity.kt

--- MyApplication123/app/src/main/java/com/example/myapplication123/ui/MainActivity.kt
+++ MyApplication123/app/src/main/java/com/example/myapplication123/ui/MainActivity.kt
@@ -10,8 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        initBottomNavigation()
     }
-    
+
 }
