54c703148d472d437f879eca2990ce956df80c92
김현재 <41482946+galug@users.noreply.github.com>
2022-07-06 11:11:57 +0900

    2022-07-06-김현재 bottomnavigation 추가

---
+++ MyApplication123/app/src/main/java/com/example/myapplication123/ui/FragmentOne.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication123.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication123.R
+import com.example.myapplication123.databinding.Fragment1Binding
+
+
+class FragmentOne : Fragment() {
+    lateinit var binding: Fragment1Binding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_1, container, false)
+    }
+
+}
