d118278ad07d6dcd63e7e4406720fd1ee011605f
김현재 <41482946+galug@users.noreply.github.com>
2022-07-10 04:56:53 +0900

    07-10 04:56

--- JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/MyPageFragment.kt
+++ JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/MyPageFragment.kt
@@ -5,18 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.jackpot.jackpotfront.R
+import com.jackpot.jackpotfront.adapter.ListAdapterGrid
 import com.jackpot.jackpotfront.databinding.FragmentMyPageBinding
+import com.jackpot.jackpotfront.retrofit.data.TestPostObject
 
 class MyPageFragment : Fragment() {
 
     lateinit var binding: FragmentMyPageBinding
+    var userIdx: Int? = 1
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMyPageBinding.inflate(layoutInflater,container,false)
-        // Inflate the layout for this fragment
+
+        var testObject: ArrayList<TestPostObject> = ArrayList()
+        var postObject: TestPostObject = TestPostObject(1,
+            "https://hana-umc.shop/test/display/1657267165243img.jpg",
+            "testStringqfi;ljqwqwfj;jiwqfj;wfq;jilqwflfuawfhawklff;lqwfl;qwjifqfwafawfajil;fwa;fjil;wf")
+        testObject?.add(postObject)
+
+        ////// 그리드 뷰로 화면 출력 //////
+        val listManager = GridLayoutManager(context,1)
+        var listAdapter = ListAdapterGrid(context, userIdx, testObject)
+
+        var recyclerList = binding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = listManager
+            adapter = listAdapter
+        }
+
         return binding.root
     }
 
