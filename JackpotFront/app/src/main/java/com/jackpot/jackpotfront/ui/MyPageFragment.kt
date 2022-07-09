e08b4c79a3beea518dfc3b62540175c0a3dd19d2
김현재 <41482946+galug@users.noreply.github.com>
2022-07-10 08:09:00 +0900

    07-10 08:08 김현재

--- JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/MyPageFragment.kt
+++ JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/MyPageFragment.kt
@@ -130,34 +130,6 @@ class MyPageFragment : Fragment() {
                 })
             }
 
-            holder.binding.favButton.setOnClickListener {
-
-                if(!isHearting){
-                    //기본이 false이므로 false가 아닐때 실행한다.
-                    //애니메이션의 커스텀
-                    //0f가 0퍼센트, 1F가 100퍼센트
-                    //ofFloat(시작지점, 종료지점).setDuration(지속시간)
-                    // Custom animation speed or duration.
-                    val animator = ValueAnimator.ofFloat(0f, 0.5f).setDuration(500)
-                    animator.addUpdateListener {
-                        holder.binding.favButton.progress = it.animatedValue as Float
-                    }
-                    animator.start()
-                    isHearting = true // 그리고 트루로 바꾼다.
-                    Log.d("MYTAG", "MainActivity - onClickButton() called / 좋아요 버튼이 클릭됨")
-                }else{
-                    //트루일때가 실행된다.
-                    val animator = ValueAnimator.ofFloat(0.5f, 1f).setDuration(500)
-                    animator.addUpdateListener {
-                        holder.binding.favButton.progress = it.animatedValue as Float
-                    }
-                    animator.start()
-                    isHearting = false // 다시 false로 된다.
-                    Log.d("MYTAG", "MainActivity - onClickButton() called / 좋아요 버튼이 꺼짐")
-                }
-
-                Log.d("MYTAG", "어댑터에서는.. " + isHearting.toString())
-            }
         }
 
 
