ef9f768de7629da3636ed3d1d222d0ba95852932
sonsh0417 <okgnb@naver.com>
2022-07-09 17:04:44 +0900

    modify testcontroller

--- jackpot/src/main/java/hackathon/jackpot/test/TestController.java
+++ jackpot/src/main/java/hackathon/jackpot/test/TestController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
 
     @ResponseBody
-    @GetMapping("/log")
-    public String test(){
+    @GetMapping("/log/{userIdx1}/{userIdx2}")
+    public String test(@PathVariable int userIdx1, @PathVariable int userIdx2){
+        System.out.println("userIdx1 : "+userIdx1+"     userIdx2 : " + userIdx2);
 
         return "테스트 성공 in jackpot";
     }
