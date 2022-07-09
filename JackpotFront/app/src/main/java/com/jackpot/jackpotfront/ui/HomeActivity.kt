9d5831f9cf515387c6cb7aeb9abe046025dc9384
김현재 <41482946+galug@users.noreply.github.com>
2022-07-09 16:01:49 +0900

    2022-07-09-16:01 김현재 nav 제거

--- JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/HomeActivity.kt
+++ JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/HomeActivity.kt
@@ -12,37 +12,6 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 
-    private fun initBottomNavigation() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_frm, OneFragment())
-            .commitAllowingStateLoss()
-        binding.mainBnv.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.postFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, OneFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.clothesFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, TwoFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.myPageFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, ThreeFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-
-                }
-            }
-            false
-        }
-    }
 }
