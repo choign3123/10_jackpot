b29f9c0902e0943eacee827ceeb2ee6f9122f091
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2022-07-09 20:30:54 +0900

    0709 20:30 레트로핏 로그인, 게시물 생성 추가

---
+++ JackpotFront/app/src/main/java/com/jackpot/jackpotfront/retrofit/data/LoginInfo.kt
@@ -0,0 +1,15 @@
+package com.jackpot.jackpotfront.retrofit.data
+
+data class LoginInfo(
+    val id: String,
+    val pw: String
+)
+
+data class LoginResult(
+    val isSuccess: Boolean,
+    val result: LoginObject
+)
+
+data class LoginObject(
+    val userIdx: Int
+)
--- JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/PostFragment.kt
+++ JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/PostFragment.kt
@@ -23,7 +23,6 @@ class PostFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         var testObject: ArrayList<AllPostsObject>? = null
 
 
