0a214507b295659c9ec7a04b1a3da148c1da1b82
HyunjeLee <101737272+HyunjeLee@users.noreply.github.com>
2022-07-09 16:51:19 +0900

    16:50 이현제
    리사이클러뷰, 레트로핏 추가

--- JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/MainActivity.kt
+++ JackpotFront/app/src/main/java/com/jackpot/jackpotfront/ui/MainActivity.kt
@@ -2,14 +2,31 @@ package com.jackpot.jackpotfront.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.jackpot.jackpotfront.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 모든 게시물 조회
+        getAllPosts()
+
+    }
+
+    private fun getAllPosts() {
+        // 레트로핏 통신
+        val listManager = GridLayoutManager(this@MainActivity, 1)
+        var listAdapter = ListAdapterGrid(this@MainActivity, userIdx, response.body()!!.result)
+
+        var recyclerList = binding.gridView.apply {
+            setHasFixedSize(true)
+            layoutManager = listManager
+            adapter = listAdapter
+        }
     }
 }
