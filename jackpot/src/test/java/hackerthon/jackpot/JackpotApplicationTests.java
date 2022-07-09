717b2baf487eccabc3d87e200fe6c4bcb2066e0b
sonsh0417 <okgnb@naver.com>
2022-07-09 10:55:44 +0900

    initial commit

---
+++ jackpot/src/test/java/hackerthon/jackpot/JackpotApplicationTests.java
@@ -0,0 +1,13 @@
+package hackerthon.jackpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
